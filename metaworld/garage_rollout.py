518fbb8646718a81c14fa64eaa78456e552499ab
Filip Nilsson <filip.c.nilsson@gmail.com>
2023-02-08 13:44:29 +0100

    can run garage policy with mujoco and garage code :)

--- metaworld/garage_rollout.py
+++ metaworld/garage_rollout.py
@@ -2,7 +2,7 @@
 from garage.experiment import Snapshotter
 
 snapshotter = Snapshotter()
-data = snapshotter.load("/home/fleip/research/meta-rl/metaworld/experiment/maml_trpo_metaworld_ml10_seed=1_epochs=3_rollouts_per_task=10_meta_batch_size=20_inner_lr=0.0001")
+data = snapshotter.load("/home/fleip/research/meta-rl/metaworld/experiment/maml_trpo_metaworld_ml10_seed=1_epochs=2000_rollouts_per_task=10_meta_batch_size=20_inner_lr=0.0001_6")
 policy = data['algo'].policy
 
 # You can also access other components of the experiment
@@ -10,5 +10,5 @@ env = data['env']
 
 # See what the trained policy can accomplish
 from garage import rollout
-path = rollout(env, policy, animated=True)
+path = rollout(env, policy,pause_per_frame=0.025, animated=True)
 print(path)
